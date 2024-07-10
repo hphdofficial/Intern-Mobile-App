882df949005c479f1dd264e5a47a7a1410844e49
hphdofficial <hphdofficial@gmail.com>
2024-07-11 02:41:51 +0700

    Create mock data and update UI for stored item, shipping item, purchase history

--- Mobile/app/src/main/java/com/android/mobile/ShippingItemActivity.java
+++ Mobile/app/src/main/java/com/android/mobile/ShippingItemActivity.java
@@ -7,6 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.mobile.adapter.ShippingItemAdapter;
+import com.android.mobile.adapter.StoredItemAdapter;
+
+import java.util.ArrayList;
 
 public class ShippingItemActivity extends AppCompatActivity {
 
@@ -20,5 +29,25 @@ public class ShippingItemActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        titleFragment newFragment = new titleFragment();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+        fragmentTransaction.replace(R.id.fragment_container, newFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+        ArrayList<String> data = new ArrayList<>();
+        data.add("Sản phẩm 1");
+        data.add("Sản phẩm 2");
+        data.add("Sản phẩm 3");
+        data.add("Sản phẩm 4");
+        data.add("Sản phẩm 5");
+
+        ShippingItemAdapter shippingItemAdapter = new ShippingItemAdapter(this, data);
+        RecyclerView recyclerView = findViewById(R.id.recycler_shipping_item);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(shippingItemAdapter);
     }
 }
