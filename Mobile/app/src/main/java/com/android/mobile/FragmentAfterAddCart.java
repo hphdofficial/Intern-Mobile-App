9c93a761c8ae883bbd0d891d174909579c5e9b7c
Thuy Tang <tangxuanthuy11122002@gmail.com>
2024-08-12 11:26:04 +0700

    fix Theory

--- Mobile/app/src/main/java/com/android/mobile/FragmentAfterAddCart.java
+++ Mobile/app/src/main/java/com/android/mobile/FragmentAfterAddCart.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.android.mobile.models.OrderStatusModel;
+//import com.android.mobile.models.OrderStatusModel;
 import com.google.gson.Gson;
 
 public class FragmentAfterAddCart extends Fragment {
